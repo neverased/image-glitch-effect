29d6910eb555c0bf2fcf60bde8a380f3595aaffa
Wojciech Bajer <mail@wojciechbajer.com>
2023-10-15 14:31:41 +0200

    Refactor
    Project refactored to Typescript
    React Component

---
+++ src/components/ImageGlitch/glitch-image.tsx
@@ -0,0 +1,100 @@
+import classNames from 'classnames';
+import { pickRandomFromRange } from '../../../utils/utils';
+import React, { useEffect } from 'react';
+import 
+
+const INITIAL_WAIT_TIME = 1000;
+const CLEAR_TIME = 100;
+const MAX_GLITCH_COUNT = 1;
+const MAX_GLITCH_SEQUENCE_COUNT = 3;
+const MAX_GLITCH_FREQUENCY_MS = 2000;
+const MAX_GLITCH_WIDTH = 5;
+const OFFSET_GLITCH_WIDTH = 5;
+const MAX_GLITCH_LEFT = 20;
+
+export interface ImageGlitchProps {
+  src: string;
+}
+
+const ImageGlitch = ({ src }: ImageGlitchProps) => {
+  const [srcImage, setSrcImage] = React.useState(src);
+  const [isEnabled, setIsEnabled] = React.useState(false);
+  const [glitchCount, setGlitchCount] = React.useState(1);
+  const [glitchSequenceCount, setGlitchSequenceCount] = React.useState(1);
+  const [glitchWidth, setGlitchWidth] = React.useState([] as number[]);
+  const [glitchLeft, setGlitchLeft] = React.useState([] as number[]);
+  const [glitchTop, setGlitchTop] = React.useState([] as number[]);
+
+  function makeGlitch() {
+    for (let i = 0; i < glitchCount; i++) {
+      glitchWidth[i] = Math.random() * MAX_GLITCH_WIDTH + OFFSET_GLITCH_WIDTH;
+      glitchTop[i] = pickRandomFromRange(
+        i === 0 ? 0 : glitchTop[i - 1] + glitchWidth[i - 1],
+        100 - (MAX_GLITCH_WIDTH + OFFSET_GLITCH_WIDTH),
+      );
+      glitchLeft[i] = Math.random() * (2 * MAX_GLITCH_LEFT) - MAX_GLITCH_LEFT;
+    }
+
+    return { glitchWidth, glitchTop, glitchLeft };
+  }
+
+  function clearGlitch() {
+    setGlitchWidth([]);
+    setGlitchTop([]);
+    setGlitchLeft([]);
+  }
+
+  function toggleGlitch() {
+    setIsEnabled(!isEnabled);
+  }
+
+  function startGlitch() {
+    if (!isEnabled) {
+      return;
+    }
+
+    const glitchWaitTime: number[] = [];
+    setGlitchCount(Math.floor(Math.random() * MAX_GLITCH_COUNT) + 1);
+    setGlitchSequenceCount(
+      Math.floor(Math.random() * MAX_GLITCH_SEQUENCE_COUNT) + 1,
+    );
+
+    for (let i = 0; i < glitchSequenceCount; i++) {
+      const delaySeed = i + 1;
+      glitchWaitTime[i] = Math.random() * (100 * delaySeed) + 100;
+
+      setTimeout(() => {
+        setGllitch(makeGlitch());
+      }, glitchWaitTime[i]);
+    }
+
+    const maxWaitTime = Math.max.apply(null, glitchWaitTime);
+    const clearTime = maxWaitTime + CLEAR_TIME;
+
+    setTimeout(() => {
+      clearGlitch();
+    }, clearTime);
+
+    setTimeout(
+      () => {
+        startGlitch();
+      },
+      Math.random() * MAX_GLITCH_FREQUENCY_MS + clearTime,
+    );
+  }
+
+  function setGllitch({
+    glitchWidth,
+    glitchTop,
+    glitchLeft,
+  }: {
+    glitchWidth: number[];
+    glitchTop: number[];
+    glitchLeft: number[];
+  }) {
+    setGlitchWidth(glitchWidth);
+    setGlitchTop(glitchTop);
+    setGlitchLeft(glitchLeft);
+  }
+
+};
---
+++ utils/utils.spec.ts
@@ -0,0 +1,29 @@
+import { format, pickRandomFromRange } from './utils';
+
+describe('format', () => {
+    it('returns empty string for no names defined', () => {
+        expect(format('', '', '')).toEqual('');
+    });
+
+    it('formats just first names', () => {
+        expect(format('Joseph', '', '')).toEqual('Joseph');
+    });
+
+    it('formats first and last names', () => {
+        expect(format('Joseph', '', 'Publique')).toEqual('Joseph Publique');
+    });
+
+    it('formats first, middle and last names', () => {
+        expect(format('Joseph', 'Quincy', 'Publique')).toEqual(
+            'Joseph Quincy Publique'
+        );
+    });
+});
+
+describe('pickRandomFromRange', () => {
+    it('returns a number between 1 and 10', () => {
+        const random = pickRandomFromRange(1, 10);
+        expect(random).toBeGreaterThanOrEqual(1);
+        expect(random).toBeLessThanOrEqual(10);
+    });
+});
